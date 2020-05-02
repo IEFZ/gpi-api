fcc7dd121bb133b964826f3eeb68ae18c4da0497
IEFZ <54610210+IEFZ@users.noreply.github.com>
2020-05-01 20:50:40 -0400

    Add files via upload

---
+++ app/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+
+const db = require("./app/models");
+db.sequelize.sync();
+
+
+
+//analizar solicitudes de tipo de contenido - application/json
+app.use(bodyParser.json());
+
+//analizar solicitudes de tipo de contenido - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ mensaje: "gpi-dev pruebas de aplicacion." });
+});
+
+
+
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+require("./app/routes/turorial.routes")(app);
+app.listen(PORT, () => {
+  console.log(`Servidor activo en el puerto ${PORT}.`);
+});
+
